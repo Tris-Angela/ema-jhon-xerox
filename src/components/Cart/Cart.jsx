363d0cff216b5c83b35b2d8b4e0f0614d514d269
Tris-Angela <fahminakashpi95@gmail.com>
2023-03-31 15:17:12 +0600

    showed products

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,16 +4,19 @@ import './Cart.css';
 const Cart = ({cart}) => {
     let total = 0;
     let shippingCharge = 0;
+    let quantity = 0
     for(const product of cart){
-        total = total + product.price;
+        product.quantity = product.quantity||1;
+        total = total + product.price*product.quantity;
         shippingCharge = shippingCharge + product.shipping;
+        quantity = quantity + product.quantity
     }
     const vat = total*5/100;
     const totalAmount = total + shippingCharge + vat;
     return (
         <div className='cart'> 
             <h4>Order Summary </h4>
-            <p>Selected items: {cart.length}</p>
+            <p>Selected items: {quantity}</p>
             <p>Total price: ${total}</p>
             <p>Total shipping cost: ${shippingCharge}</p>
             <p>VAT: ${vat.toFixed(2)}</p>
